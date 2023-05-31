d2229e497d3c3ba7c607a436d4c8000f897c9fbd
lauraespina <42917361+lauraespina@users.noreply.github.com>
2023-05-31 11:36:57 +0200

    Changed default interval values
    To get the last 6 WHOLE months

--- f-top100customers.sql
+++ f-top100customers.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION get_top_100customers(startdate timestamp DEFAULT date_trunc('MONTH', now() - INTERVAL '5' MONTH), enddate timestamp DEFAULT date_trunc('day', NOW()))
+CREATE OR REPLACE FUNCTION get_top_100customers(startdate timestamp DEFAULT date_trunc('MONTH', NOW() - INTERVAL '6 month'), enddate timestamp DEFAULT date_trunc('MONTH', NOW()) - INTERVAL '1 day')
 	RETURNS TABLE(customer_id int, customer_nr int, nameperson varchar, email varchar, country varchar, city varchar, spent numeric, nr_transactions bigint, last_transaction timestamp)
 	LANGUAGE plpgsql
 AS $$
 BEGIN
 	RETURN query
-	WITH toptr AS (
+	WITH custotals AS (
 		SELECT
 			coo.customer_id AS "customer_id",
 			sum(coo.total) AS "spent",
@@ -16,21 +16,21 @@ BEGIN
 		ORDER BY "spent" DESC
 		)
 	SELECT
-		toptr.customer_id::int,
+		custotals.customer_id::int,
 		cucu.customer_number::int,
 		CONCAT(cucu.first_name, ' ', cucu.last_name)::varchar,
 		cucu.email::varchar,
 		ac.name::varchar,
 		aa.city::varchar,
-		toptr.spent::numeric,
-		toptr.nr_transactions::bigint,
-		toptr.last_transaction::timestamp
-	FROM toptr
-	LEFT JOIN customer.customers cucu ON toptr.customer_id = cucu.id
-	LEFT JOIN reporting.preferred_addresses_view pa ON toptr.customer_id = pa.customer_id
+		custotals.spent::numeric,
+		custotals.nr_transactions::bigint,
+		custotals.last_transaction::timestamp
+	FROM custotals
+	LEFT JOIN customer.customers cucu ON custotals.customer_id = cucu.id
+	LEFT JOIN reporting.preferred_addresses_view pa ON custotals.customer_id = pa.customer_id
 	LEFT JOIN address.addresses aa ON pa.delivery_address_id = aa.id
 	LEFT JOIN address.countries ac ON aa.country_id = ac.id
-	WHERE toptr.customer_id NOT IN (SELECT re.customer_id FROM reporting.exclusions re)
+	WHERE custotals.customer_id NOT IN (SELECT re.customer_id FROM reporting.exclusions re)
 	ORDER BY spent DESC
 	LIMIT 100;
 END;
